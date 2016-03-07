3f8c560afc2e622b085f1a7c0ca8ffba24824778
kyoungchin <kcseo.acm@gmail.com>
2016-03-07 12:25:02 +0900

    request/response index.html retrieve user data from signup page

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,22 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,11 +32,63 @@ public class RequestHandler extends Thread {
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			InputStreamReader isr = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(isr);
 			
-			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "I am happy to remind (~: home, .:current ..:parent). Korean Chareacters are not displayed. Why?".getBytes();
-			response200Header(dos, body.length);
-			responseBody(dos, body);
+			String line = br.readLine();
+			if (line == null) {
+				return;
+			}
+			System.out.println(line);
+			// split first line including file
+			String [] tokens = line.split(" ");
+			String url = tokens[1];
+			if (url.equals("/")) {
+				return;
+			}
+			
+			int index = url.indexOf("?");
+			System.out.println("index:" + index);
+			if (index > -1) { 
+			
+				String requestPath = url.substring(0,index);
+				String params = url.substring(index+1);
+				
+				//HttpRequestUtils hru = new HttpRequestUtils();
+				Map<String,String> data = new HashMap<String,String>();
+				data = HttpRequestUtils.parseQueryString(params);
+				
+				User user = new User(data.get("userId"), data.get("password"),data.get("name"),data.get("email"));
+				
+				System.out.println(user);
+				
+				String ID = data.get("userId");
+				System.out.println("ID= " + ID);
+				DataOutputStream dos = new DataOutputStream(out);
+				byte[] body = user.toString().getBytes();
+				response200Header(dos, body.length);
+				responseBody(dos, body);
+			} else if (url.equals("/favicon.ico")) {
+				return;
+			} else {
+				byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+				DataOutputStream dos = new DataOutputStream(out);
+				response200Header(dos, body.length);
+				responseBody(dos, body);
+			}	
+			
+			
+			
+			
+			
+			
+		
+			
+			
+			
+			
+			
+			
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
