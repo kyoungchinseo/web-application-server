6a49b9a474a18d8d75727a3de8a2debcbc42f779
kyoungchin <kcseo.acm@gmail.com>
2016-03-08 18:57:25 +0900

    refactoring (httpRequest/httpRespons 클래스로 나눔 )

---
+++ src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,253 @@
+package webserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
+
+public class HttpRequest {
+	
+	public HttpRequest(BufferedReader br) {
+
+	}
+
+	public Map<String, String> getHTTPMap(BufferedReader br) {
+		// TODO Auto-generated method stub
+		String line;
+		Map<String, String> http = new HashMap<String, String>();
+		try {
+			while(!((line = br.readLine()).equals(""))) {
+				String[] splitLine = line.split(": ");
+				if (splitLine.length == 1) {
+					String []tk = line.split(" ");
+					http.put("METHOD", tk[0]);
+					http.put("url",tk[1]);
+				} else if (splitLine.length >= 2){
+					http.put(splitLine[0],splitLine[1]);
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return http;
+	}
+	
+	public boolean isGETMethod(Map<String,String> http) {
+		return http.get("METHOD").equals("GET");
+	}
+	
+	public boolean isPOSTMethod(Map<String,String> http) {
+		return http.get("METHOD").equals("POST");
+	}
+	
+	public void respond(BufferedReader br, Map<String,String> http, HttpResponse httpResponse) {
+		if (isFavicon(http)) {
+			return;
+		}
+		if (isGETMethod(http)) {
+			respondGET(http, httpResponse);
+		}
+		if (isPOSTMethod(http)) {
+			respondPOST(br, http,httpResponse);
+		}
+	}
+
+	private boolean isFavicon(Map<String, String> http) {
+		return http.get("url").equals("/favicon.ico");
+	}
+
+	private void respondGET(Map<String, String> http, HttpResponse httpResponse) {
+		String url = http.get("url");
+		
+		int index = url.indexOf("?");  // if no ?, -1 
+		if (index > -1) { // request from GET method with user data
+			String requestPath = url.substring(0,index);
+			String params = url.substring(index+1);
+			
+			Map<String,String> data = new HashMap<String,String>();
+			data = HttpRequestUtils.parseQueryString(params);	
+			User user = new User(data.get("userId"), data.get("password"),data.get("name"),data.get("email"));
+			System.out.println("USER: " + user);
+			
+			// ADD USER to STATIC RAM MEMORY
+			DataBase.addUser(user);
+			
+			
+			// request
+			httpResponse.response202(url, user.toString());
+			
+			return;
+		} 
+		
+		if (url.equals("/user/list")) {
+			String cookie = http.get("Cookie");
+			boolean logined = cookie.contains("logined=true");
+			System.out.println("LOGIN: " + logined);
+			if (logined == true) {
+				//url = "/user/list.html";
+				//byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+				StringBuilder html = new StringBuilder();
+				html.append("<!DOCTYPE html>");
+				html.append("<html lang=&quotkr&quot>");
+				html.append("<head>");
+				html.append("<meta http-equiv=&quotcontent-type&quot content=&quottext/html; charset=UTF-8&quot>");
+				html.append("<meta charset=&quotutf-8&quot>");
+				html.append("<title>SLiPP Java Web Programming</title>");
+				html.append("<meta name=&quotviewport&quot content=&quotwidth=device-width, initial-scale=1, maximum-scale=1&quot>");
+				html.append("<link href=&quot../css/bootstrap.min.css&quot rel=&quotstylesheet&quot>");
+				html.append("<link href=&quot../css/styles.css&quot rel=&quotstylesheet&quot>");
+				html.append("</head>");
+				html.append("<body>");
+				
+				html.append("<table>");
+				html.append("<thead>");
+				html.append("<tr>");
+				html.append("<th>USER ID</th> <th>NAME</th> <th>EMAIL</th>");
+				html.append("</tr>");
+				html.append("</thead>");
+				html.append("<tbody>");
+				html.append("</tbody>");
+				// table and with user data
+				Collection<User> userList = DataBase.findAll();
+				Iterator it = userList.iterator();
+				while(it.hasNext()) {
+					User user = (User)it.next();
+					html.append("<tr>");
+					html.append("<td>"+user.getUserId()+"</td> <td>"+user.getName()+"</td> <td>"+user.getEmail()+"</td>");
+					html.append("</tr>");
+				}
+	            html.append("</table>");
+				
+				html.append("</body>");
+				html.append("</html>");
+				
+				
+				httpResponse.response202(url, html.toString());
+				return;
+			} 
+			
+			if (logined == false) {
+				url = "/user/login.html";
+				try {
+					httpResponse.response302(url);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return;
+			}
+		} 
+		
+		if (url.equals("/")) {
+			url = "/index.html";
+		}
+			
+		try {
+			httpResponse.response202(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	
+	}	
+	
+	private void respondPOST(BufferedReader br, Map<String, String> http, HttpResponse httpResponse) {
+		String url = http.get("url");
+		if (url.equals("/")) {
+			return;
+		}
+		
+		if (url.equals("/user/create")) {
+			int contentLength = Integer.parseInt(http.get("Content-Length"));
+			if (contentLength < 0) {
+				return;
+			} 
+			String userData = "";
+			try {
+				userData = util.IOUtils.readData(br, contentLength);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("User Data: " + userData);
+			
+			Map<String,String> data = new HashMap<String,String>();
+			data = HttpRequestUtils.parseQueryString(userData);	
+			User user = new User(data.get("userId"), data.get("password"),data.get("name"),data.get("email"));
+			System.out.println("USER: " + user);
+			
+			// ADD USER to STATIC RAM MEMORY
+			DataBase.addUser(user);
+			
+			// redirection
+			url = "/index.html";
+			try {
+				httpResponse.response302(url);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return;
+		}
+		
+		if (url.equals("/user/login")) {
+			int contentLength = Integer.parseInt(http.get("Content-Length"));
+			if (contentLength < 0) {
+				return;
+			} 
+			String userData="";
+			try {
+				userData = util.IOUtils.readData(br, contentLength);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("User Data: " + userData);
+			
+			Map<String,String> data = new HashMap<String,String>();
+			data = HttpRequestUtils.parseQueryString(userData);	
+
+		
+			// compare data
+			User user = DataBase.findUserById(data.get("userId"));
+			
+			boolean setCookie = false;
+			if (user != null && user.getPassword().equals(data.get("password"))) { // login succeeded.
+				url = "/index.html";
+				setCookie = true;
+				System.out.println(user.toString());
+			} else { // login failed.
+				url = "/user/login_failed.html";
+				setCookie = false;
+				System.out.println("NO USER DATA");
+			}
+			
+			// redirection
+			try {
+				httpResponse.response302(url,setCookie);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return;
+		}
+
+		try {
+			httpResponse.response202(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+		
+	}	
+				
+}
--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,61 +1,61 @@
 package webserver;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.Socket;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import db.DataBase;
-import model.User;
-import util.HttpRequestUtils;
 
-public class RequestHandler extends Thread {
-	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+public class HttpResponse {
+	private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
 	
-	private Socket connection;
-
-	public RequestHandler(Socket connectionSocket) {
-		this.connection = connectionSocket;
+	OutputStream out;
+	
+	public HttpResponse(OutputStream out) {
+		this.out = out;
 	}
-
-	public void run() {
-		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
-		
-		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-			
-			InputStreamReader isr = new InputStreamReader(in);
-			BufferedReader br = new BufferedReader(isr);
-			
-			HttpRequest httpRequest = new HttpRequest(br);
-			
-			// read stream data
-			Map<String,String> http = new HashMap<String,String>();
-			http = httpRequest.getHTTPMap(br);
-			System.out.println("HASHMAP: " + http.toString());
-			
-			HttpResponse httpResponse = new HttpResponse(out);
-			
-			httpRequest.respond(br, http, httpResponse);
-			
-		} catch (IOException e) {
-			log.error(e.getMessage());
+	
+	public void response202(String url) throws IOException {
+		byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+		DataOutputStream dos = new DataOutputStream(out);
+		String contentType = "text/html";
+		if (url.contains("css")) {
+			contentType = "text/css";
 		}
+		response200Header(dos, contentType, body.length);
+		responseBody(dos, body);
 	}
-
+	
+	public void response202(String url, String data) {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = data.getBytes();
+		String contentType = "text/html";
+		if (url.contains("css")) {
+			contentType = "text/css";
+		}
+		response200Header(dos, contentType, body.length);
+		responseBody(dos, body);
+	}
+	
+	
+	public void response302(String url) throws IOException {
+		byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+		DataOutputStream dos = new DataOutputStream(out);
+		response302Header(dos, url, false, body.length);
+		responseBody(dos, body);
+	}
+		
+	public void response302(String url, boolean setCookie) throws IOException {
+		byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+		DataOutputStream dos = new DataOutputStream(out);
+		response302Header(dos, url, setCookie, body.length);
+		responseBody(dos, body);
+	}
+	
 	private void response302Header(DataOutputStream dos, String url, boolean setCookie, int lengthOfBodyContent) {
 		// TODO Auto-generated method stub
 		try {
@@ -84,7 +84,7 @@ public class RequestHandler extends Thread {
 		}
 	}
 	
-	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+	private void response200Header(DataOutputStream dos,int lengthOfBodyContent) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
 			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
@@ -103,4 +103,6 @@ public class RequestHandler extends Thread {
 			log.error(e.getMessage());
 		}
 	}
+
+	
 }
